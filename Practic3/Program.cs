bca3dcddd1a1851484b72848b9979121f6b812e5
DjorikPupkin <DjorikPupkin@DJORIK>
2021-11-30 19:00:24 +0300

    Добавьте файлы проекта.

---
+++ Practic3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practic3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работа с гитхабом");
+        }
+    }
+}
